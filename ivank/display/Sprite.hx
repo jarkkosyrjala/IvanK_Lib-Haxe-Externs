36c2a4b2e8538ea9332dc27ef6d30f4be5c73a8d
Jarkko Syrjälä <j@rkko.net>
2013-07-03 08:56:38 +0300

    Small fixes

--- ivank/display/Sprite.hx
+++ ivank/display/Sprite.hx
@@ -1,5 +1,6 @@
 package ivank.display;
 import ivank.display.DisplayObjectContainer;
+import ivank.display.Graphics;
 @:native("window.Sprite")
 extern class Sprite extends DisplayObjectContainer {
 public function new():Void;
--- ivank/events/Event.hx
+++ ivank/events/Event.hx
@@ -6,13 +6,13 @@ extern class Event {
     var currentTarget:EventDispatcher;
     var target:EventDispatcher;
     var type:String;
-    static var ENTER_FRAME:String = "enterFrame";
-    static var COMPLETE:String = "complete";
-    static var CHANGE:String = "change";
-    static var RESIZE:String = "resize";
-    static var ADDED_TO_STAGE:String = "addedToStage";
-    static var REMOVED_FROM_STAGE:String = "removedFromStage";
+    static inline var ENTER_FRAME:String = "enterFrame";
+    static inline var COMPLETE:String = "complete";
+    static inline var CHANGE:String = "change";
+    static inline var RESIZE:String = "resize";
+    static inline var ADDED_TO_STAGE:String = "addedToStage";
+    static inline var REMOVED_FROM_STAGE:String = "removedFromStage";
     //Undocumented:
-    static var OPEN:String = "open";
-    static var PROGRESS:String = "progress";
+    static inline var OPEN:String = "open";
+    static inline var PROGRESS:String = "progress";
 }
